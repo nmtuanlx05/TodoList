edb1a7ceb7e60b0056aa1f02cc34de7460579ffb
nmtuanlx05 <manhtuanlx2005@gmail.com>
2025-07-23 16:34:40 +0700

    finish complete task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,13 @@
-export default function TodoItem({todo,index,removeTodo}) {
+export default function TodoItem({ todo, onToggle, onRemove  }) {
     return (
-        <li key={index} className=" bg-blue-100 flex justify-between items-center mt-2 mb-2  border h-9">                          
-            <span className="ml-2">{todo}</span>
-            <button onClick={() => removeTodo(index)}
+        <li  className={` bg-blue-100 flex justify-between items-center mt-2 mb-2  border h-9  ${ todo.completed ? "opacity-50 " : ""} `}>   
+            <input type="checkbox"
+                className="w-8 h-4 {} "
+                checked={todo.completed}
+                onChange={onToggle}
+            />    
+            <span className="ml-2">{todo.text}</span>
+            <button onClick={onRemove}
                 className="bg-red-400 px-1.5 rounded h-full hover:bg-red-500 ">
                 Delete
             </button>
